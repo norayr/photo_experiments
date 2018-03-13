154d99a9fb292b7840f5f223aa46ef76fa1826dd
norayr <norayr@arnet.am>
2018-03-13 15:44:47 +0400

    a

--- get.sh
+++ get.sh
@@ -1,4 +1,6 @@
 wget -c 'http://www.fmwconcepts.com/imagemagick/downloadcounter.php?scriptname=curves&dirname=curves' -O curves
 wget -c 'http://www.fmwconcepts.com/imagemagick/downloadcounter.php?scriptname=curves&dirname=saturation' -O saturation
+wget -c 'http://www.fmwconcepts.com/imagemagick/downloadcounter.php?scriptname=autowhite&dirname=autowhite' -O autowhite
 chmod 777 curves
 chmod 777 saturation
+chmod 777 autowhite
