76bf8533a4d5cebf5c47f569cb77b05693917c2d
norayr <norayr@arnet.am>
2018-10-25 13:48:26 +0400

    աննշան

--- get.sh
+++ get.sh
@@ -1,7 +1,7 @@
 wget -c 'http://www.fmwconcepts.com/imagemagick/downloadcounter.php?scriptname=curves&dirname=curves' -O curves
 wget -c 'http://www.fmwconcepts.com/imagemagick/downloadcounter.php?scriptname=curves&dirname=saturation' -O saturation
 wget -c 'http://www.fmwconcepts.com/imagemagick/downloadcounter.php?scriptname=autowhite&dirname=autowhite' -O autowhite
-wget -c 'http://www.fmwconcepts.com/imagemagick/downloadcounter.php?scriptname=autolevel&dirname=autolevel' -O autowhite
+wget -c 'http://www.fmwconcepts.com/imagemagick/downloadcounter.php?scriptname=autolevel&dirname=autolevel' -O autolevel
 chmod 777 curves
 chmod 777 saturation
 chmod 777 autowhite
--- imagemagick/pre_instax_sp2/test.sh
+++ imagemagick/pre_instax_sp2/test.sh
@@ -1,6 +1,6 @@
 for i in *.jpg
 do
   bn=$(basename ${i%.*})
-  autolevel -c luminance $i $bn_sp2.jpg
+  autolevel -c luminance $i ${bn}_sp2.jpg
 
 done
