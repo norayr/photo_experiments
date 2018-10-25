1fb9249855650e1f9b6e0c4b318b9bb07d1b7a56
norayr <norayr@arnet.am>
2018-10-25 17:46:08 +0400

    note on lcms support

--- imagemagick/pre_instax_sp2/test.sh
+++ imagemagick/pre_instax_sp2/test.sh
@@ -1,7 +1,10 @@
+#requires
+#media-gfx/imagemagick lcms
+
 export PATH="~/bin:$PATH"
 ext="sp2"
 
-src_prf="sRGB"
+#src_prf="sRGB"
 dst_prf="SP2_Instax_Color_Ver_1-00.icc"
 
 list=`ls *.jpg`
@@ -14,9 +17,16 @@ do
   #identify the color profile
   src_prf=`identify -format '%[colorspace]' $i`
   echo "identified color profile is $src_prf"
+  if [ "$src_prf" == "sRGB" ]
+  then
+    src_prf_file="sRGB_Color_Space_Profile.ICM"
+  else
+    #TODO
+    src_prf_file="sRGB_Color_Space_Profile.ICM"
+  fi
   autolevel -c average $i ${bn}_${ext}_tmp.jpg
   convert ${bn}_${ext}_tmp.jpg +level %5,%95 -quality 100 ${bn}_${ext}.jpg
-  convert ${bn}_${ext}.jpg -profile $src_prf -profile $dst_prf -quality 100 ${bn}_${ext}_sp2iccprf.jpg
+  convert ${bn}_${ext}.jpg -profile $src_prf_file -profile $dst_prf -quality 100 ${bn}_${ext}_sp2iccprf.jpg
   #rm ${bn}_${ext}_tmp.jpg
 done
 
