ffb33907b754351929dcb547c2d0cf4c6e8cf413
norayr <norayr@arnet.am>
2018-10-25 15:37:35 +0400

    output levels

--- imagemagick/pre_instax_sp2/test.sh
+++ imagemagick/pre_instax_sp2/test.sh
@@ -1,7 +1,14 @@
 export PATH="~/bin:$PATH"
-for i in *.jpg
-do
-  bn=$(basename ${i%.*})
-  autolevel -c luminance $i ${bn}_sp2.jpg
+ext="sp2"
 
+list=`ls *.jpg`
+#list=`ls *.jpg | grep -v sp2`
+
+for i in $list
+do
+  echo $i
+  bn=$(basename ${i%.*})
+  autolevel -c average $i ${bn}_${ext}_tmp.jpg
+  convert ${bn}_${ext}_tmp.jpg +level %5,%95 -quality 100 ${bn}_${ext}.jpg
+  rm ${bn}_${ext}_tmp.jpg
 done
