c94a48d70f8900e2bd888e06f80bf4f76916c66a
Norayr Chilingarian <norayr@arnet.am>
2019-07-04 18:56:59 +0400

    if argument supplied script works for one file

--- xmp/to_all.sh
+++ xmp/to_all.sh
@@ -1,14 +1,27 @@
 set -x
 ext=jpg
 
-for i in *.$ext
-do
-
-j="${i%.*}"
+# gets one filename to convert to all xmp styles.
+toall () {
+j="${1%.*}"
   for k in *.xmp
   do
     l="${k%.*}"
-    darktable-cli $i $k ${j}_${l}.${ext}
+    darktable-cli $1 $k ${j}_${l}.${ext}
   done
-done
+
+}
+
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied, applying styles to all files"
+    for i in *.$ext
+    do
+        toall $i
+    done
+else
+        echo "got the argument $1, applying styles to that file"
+        toall $1
+fi
+
 
